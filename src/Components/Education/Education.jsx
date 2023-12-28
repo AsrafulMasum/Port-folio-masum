50a743ac85223d02fb07c11a34c08af3665ef6e3
AsrafulMasum <h.m.asrafulmasum@gmail.com>
2023-12-28 21:39:05 +0600

    about section animation done

--- src/Components/Education/Education.jsx
+++ src/Components/Education/Education.jsx
@@ -23,6 +23,29 @@ const textVariants = {
   },
 };
 
+const eduVariants = {
+  initial: {
+    x: 100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.4,
+    },
+  },
+  scrollButton: {
+    opacity: 0,
+    y: 15,
+    transition: {
+      duration: 2,
+      repeat: Infinity,
+    },
+  },
+};
+
 const Education = () => {
   return (
     <div className="max-w-[1080px] mx-auto mt-16 flex flex-col lg:flex-row gap-10">
@@ -52,10 +75,12 @@ const Education = () => {
           Contact Me
         </motion.a>
       </motion.div>
-      <div className=" flex-1">
-        <h2 className="text-center text-7xl mb-4">Education</h2>
-        <div className="space-y-4">
-          <div className="flex flex-col gap-2 bg-slate-500 px-8 py-4 rounded-sm hover:scale-x-110 duration-1000 text-white">
+      <motion.div initial="initial"
+        whileInView="animate" variants={eduVariants} className="flex-1">
+        <motion.h2 className="text-center text-7xl mb-4">Education</motion.h2>
+        <motion.div className="space-y-4" variants={eduVariants}>
+          <motion.div variants={eduVariants} className="flex flex-col gap-2 border border-white hover:bg-[#D3D3D3] hover:text-black px-8 py-4 rounded-sm text-white">
+            <div className="hover:scale-105 duration-1000">
             <h2 className="font-bold">
               B.Sc in Computer Science & Engineering
             </h2>
@@ -68,9 +93,11 @@ const Education = () => {
               <p className="font-extralight">CGPG : 3.00</p>
             </div>
             <p>Location : Dhanmondi, Dhaka, BanglaDesh</p>
-          </div>
+            </div>
+          </motion.div>
 
-          <div className="flex flex-col gap-2 bg-slate-500 px-8 py-4 rounded-sm hover:scale-110 duration-1000 text-white">
+          <motion.div variants={eduVariants} className="flex flex-col gap-2 border border-white hover:bg-[#D3D3D3] hover:text-black px-8 py-4 rounded-sm text-white">
+            <div className="hover:scale-105 duration-1000">
             <h2 className="font-bold">Higher Secondary</h2>
             <h4 className="font-extralight">Shaheed Police smrity College</h4>
             <div className="flex gap-5">
@@ -79,9 +106,11 @@ const Education = () => {
               <p className="font-extralight">GPG : 4.75</p>
             </div>
             <p>Location : Mirpur-14, Dhaka, BanglaDesh</p>
-          </div>
+            </div>
+          </motion.div>
 
-          <div className="flex flex-col gap-2 bg-slate-500 px-8 py-4 rounded-sm hover:scale-110 duration-1000 text-white">
+          <motion.div variants={eduVariants} className="flex flex-col gap-2 border border-white hover:bg-[#D3D3D3] hover:text-black px-8 py-4 rounded-sm text-white">
+            <div className="hover:scale-105 duration-1000">
             <h2 className="font-bold">Secondary</h2>
             <h4 className="font-extralight">Vashantek High School</h4>
             <div className="flex gap-5">
@@ -90,9 +119,10 @@ const Education = () => {
               <p className="font-extralight">CGPG : 4.88</p>
             </div>
             <p>Location : Dhaka-Cant., Dhaka, BanglaDesh</p>
-          </div>
-        </div>
-      </div>
+            </div>
+          </motion.div>
+        </motion.div>
+      </motion.div>
     </div>
   );
 };
