6910636e73b4fc431492c50a13ba4dad3bd9561f
AsrafulMasum <h.m.asrafulmasum@gmail.com>
2024-01-02 22:49:36 +0600

    skills animation modified

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -35,7 +35,7 @@ const Skills = () => {
       <motion.h2 variants={textVariants} className="text-center text-7xl mb-10">Skills</motion.h2>
       <motion.div variants={textVariants} className="flex flex-wrap justify-center gap-12">
         {
-          skills.map((skill, idx) => <motion.p key={idx} variants={textVariants} className="border border-gray-600 text-white px-12 py-4 inline-block rounded-sm"><p className="hover:scale-125 duration-1000">{skill}</p></motion.p>)
+          skills.map((skill, idx) => <motion.p key={idx} variants={textVariants} className="border border-gray-600 text-white px-12 py-4 inline-block rounded-sm hover:bg-[#D3D3D3] hover:text-black group"><p className="group-hover:scale-125 duration-1000">{skill}</p></motion.p>)
         }
         
       </motion.div>
